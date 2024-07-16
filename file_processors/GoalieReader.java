1701ad291bedb58a7203dc5446011594d162c34d
Andrew Sauerland <141577367+AndrewSauerland@users.noreply.github.com>
2024-07-15 21:46:17 -0400

    Rename file-processors to file_processors to comply with package naming conventions. Add GoalieReader file in package

---
+++ file_processors/GoalieReader.java
@@ -0,0 +1,67 @@
+package file_processors;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class GoalieReader {
+  
+  public String path;
+	public String tableName;
+  
+	public GoalieReader(String path, String tableName) {
+		this.path = path;
+		this.tableName = tableName;
+	}
+	
+	//Establish document headers - returns ArrayList<String> of headers from "path"
+	public ArrayList<String> getHeaders(String path) throws IOException {
+		String line = readFirstLine(path);
+		ArrayList<String> headers = new ArrayList<>();
+		String [] split = line.split(",");
+		for (String item : split) {
+			headers.add(item);
+		}
+		return headers;
+	}
+	
+	//Reads first line, returns unedited string 
+	public String readFirstLine(String path) throws IOException{
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+			String line = br.readLine();
+			return formatFirstLine(line);
+		}
+	}
+	
+	
+	public String formatFirstLine(String s) {
+		String formatted = "";
+		String[] split = s.split(",");
+		for (String word : split) {
+			formatted += word + ", ";
+		}
+		int lastComma = formatted.lastIndexOf(",");
+		formatted = formatted.substring(0, lastComma);
+		return formatted;
+	}
+	
+	//Reads the goalies sheet - returns ArrayList<ArrayList<String>> from "path"
+	public ArrayList<ArrayList<String>> readGoalieData(String path) throws IOException {
+		ArrayList<ArrayList<String>> data = new ArrayList<>();
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+			br.readLine();
+			String line = null;
+			while ((line = br.readLine()) != null) {
+				ArrayList<String> lineData = new ArrayList<>();
+				String [] broken = line.split(",");
+				for (String item : broken) {
+					lineData.add(item);
+				}
+				data.add(lineData);
+			}
+		}
+		return data;
+	}
+
+}
---
+++ file_processors/SkaterReader.java
@@ -0,0 +1,67 @@
+package file_processors;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class SkaterReader {
+
+	public String path;
+	public String tableName;
+	
+	public SkaterReader(String path, String tableName) {
+		this.path = path;
+		this.tableName = tableName;
+	}
+	
+	//Gets headers - Returns ArrayList<String> of headers from "path"
+	public ArrayList<String> getHeaders(String path) throws IOException {
+		String line = readFirstLine(path);
+		ArrayList<String> headers = new ArrayList<String>();
+		String [] split = line.split(",");
+		for (String word : split) {
+			headers.add(word);
+		}
+		return headers;
+	}
+	
+	//Reads the first line - Returns first line of "path", unedited
+	public String readFirstLine(String path) throws IOException {
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+			String line = br.readLine();
+			return formatFirstLine(line);
+		}
+	}
+	
+	
+	public String formatFirstLine(String s) {
+		String formatted = "";
+		String[] split = s.split(",");
+		for (String word : split) {
+			formatted += word + ", ";
+		}
+		int lastComma = formatted.lastIndexOf(",");
+		formatted = formatted.substring(0, lastComma);
+		return formatted;
+	}
+	
+	//Reads the skaters sheet - Returns ArrayList<ArrayList<String>> from "path"
+	public ArrayList<ArrayList<String>> readSkaterData(String path) throws IOException {
+		ArrayList<ArrayList<String>> fullData = new ArrayList<ArrayList<String>>();
+		try(BufferedReader br = new BufferedReader(new FileReader(path))) {
+			br.readLine();
+			String line = null;
+			while((line = br.readLine()) != null){
+				ArrayList<String> modifiedLine = new ArrayList<String>();
+				String [] split = line.split(",");
+				for (String word : split) {
+					modifiedLine.add(word);
+				}
+				fullData.add(modifiedLine);
+			}
+		}
+		return fullData;
+	}
+
+}
