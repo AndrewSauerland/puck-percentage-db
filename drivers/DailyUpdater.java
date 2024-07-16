d6f32877dd2dd34c413a58cc9488ac7159ae4964
Andrew Sauerland <141577367+AndrewSauerland@users.noreply.github.com>
2024-07-15 21:13:47 -0400

    Baseline functionality in driver to download specified sheets

--- drivers/DailyUpdater.java
+++ drivers/DailyUpdater.java
@@ -1,5 +1,48 @@
 package drivers;
 
+import spider.FileDownloader;
+
 public class DailyUpdater {
   
+  //Variables
+  public static String skatersSheetUrl = "https://moneypuck.com/moneypuck/playerData/seasonSummary/2023/regular/skaters.csv";
+  public static String skatersSheetPath = "lib/downloads/skaters.csv";
+
+  public static String goaliesSheetUrl = "https://moneypuck.com/moneypuck/playerData/seasonSummary/2023/regular/goalies.csv";
+  public static String goaliesSheetPath = "lib/downloads/goalies.csv";
+
+  public static String databseEndpoint = "";
+  public static String databaseUsername = "";
+  public static String databasePassword = "";
+
+  public static void main(String args[]) {
+
+    //Download skaters and 
+    downloadSheets();
+
+    //Script to parse csv files and upload to database
+
+
+
+  }
+
+
+  public static void downloadSheets() {
+
+
+    FileDownloader skatersDownloader = new FileDownloader(skatersSheetUrl, skatersSheetPath);
+    FileDownloader goaliesDownloader = new FileDownloader(goaliesSheetUrl, goaliesSheetPath);
+
+    skatersDownloader.downloadFile();
+    goaliesDownloader.downloadFile();
+
+  }
+
+  public static void updateDatabase() {
+
+    //TODO Here
+
+  }
+
+
 }
