636c782f7722ac9ebbe29baa9ebde0a1328369bc
Andrew Sauerland <141577367+AndrewSauerland@users.noreply.github.com>
2024-07-19 17:31:30 -0400

    Remove prebuild and postbuild from pipeline

--- dbConnect.java
+++ dbConnect.java
@@ -2,7 +2,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class MainTest {
+public class dbConnect {
   
   public static void main(String args[]) {
 
