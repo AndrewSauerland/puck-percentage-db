f90ba7beb3b26cfa1a9f7f40509ae531c48d8d1d
Andrew Sauerland <141577367+AndrewSauerland@users.noreply.github.com>
2024-07-21 22:38:35 -0400

    Add LocalDate identifier to test class for cloudwatch event confirmation

--- test.java
+++ test.java
@@ -1,7 +1,14 @@
+import java.time.LocalDate;
+
 public class test {
   
   public static void main(String args[]) {
     System.out.println("Hello World!");
+
+    LocalDate today = LocalDate.now();
+
+    System.out.println("Today is - " + today);
+
   }
 
 }
