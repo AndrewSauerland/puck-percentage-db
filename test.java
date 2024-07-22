72fca3cfc167841be7d851b55afde7f1df998291
Andrew Sauerland <141577367+AndrewSauerland@users.noreply.github.com>
2024-07-22 10:57:44 -0400

    Add Time to narrow down conversion

--- test.java
+++ test.java
@@ -1,11 +1,11 @@
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class test {
   
   public static void main(String args[]) {
     System.out.println("Hello World!");
 
-    LocalDate today = LocalDate.now();
+    LocalDateTime today = LocalDateTime.now();
 
     System.out.println("Today is - " + today);
 
